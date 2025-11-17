ac9934412aaef6cbe78aa6b15c1721275c54f210
William Mattsson <william.mattsson@iths.se>
2025-11-17 16:54:36 +0100

    add router and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
+import { useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 const App = () => {
-    return <div className="text-2xl text-blue-400">App</div>
+    const isOwnerPath = useLocation().pathname.includes('owner')
+
+    return <>{!isOwnerPath && <Navbar />}</>
 }
 
 export default App
