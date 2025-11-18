c16ef5f9a7153bbf6c7ec5c64f3385e35f233c6b
William Mattsson <william.mattsson@iths.se>
2025-11-18 15:01:46 +0100

    add button to featured destination section

--- client/src/components/FeatureDestination.jsx
+++ client/src/components/FeatureDestination.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from 'react-router-dom'
 import { roomsDummyData } from '../assets/assets'
 import HotelCard from './HotelCard'
 import Title from './Title'
 
 const FeatureDestination = () => {
+    const navigate = useNavigate()
+
     return (
         <div className="flex flex-col items-center px-6 md:px-16 lg:px24 bg-slate-50 py-20">
             <Title
@@ -15,6 +18,16 @@ const FeatureDestination = () => {
                     <HotelCard key={room.id} room={room} index={index} />
                 ))}
             </div>
+
+            <button
+                onClick={() => {
+                    navigate('/rooms')
+                    scrollTo(0, 0)
+                }}
+                className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer"
+            >
+                View All Destinations
+            </button>
         </div>
     )
 }
