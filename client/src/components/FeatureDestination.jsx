bb0a274d19d5f8a4dde109ee25555fc65474a375
William Mattsson <william.mattsson@iths.se>
2025-11-18 14:57:56 +0100

    add flexible title component

--- client/src/components/FeatureDestination.jsx
+++ client/src/components/FeatureDestination.jsx
@@ -1,9 +1,15 @@
 import { roomsDummyData } from '../assets/assets'
 import HotelCard from './HotelCard'
+import Title from './Title'
 
 const FeatureDestination = () => {
     return (
-        <div className="flex flex-col items-center px-6 md:px-16 lg:px24 bg-slate-50">
+        <div className="flex flex-col items-center px-6 md:px-16 lg:px24 bg-slate-50 py-20">
+            <Title
+                title="Featured Destination"
+                subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences."
+            />
+
             <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
                 {roomsDummyData.slice(0, 4).map((room, index) => (
                     <HotelCard key={room.id} room={room} index={index} />
