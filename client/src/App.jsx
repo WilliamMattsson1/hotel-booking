ad309403495759bbbcb71a9afa7fcd7d97ae1d3c
William Mattsson <william.mattsson@iths.se>
2025-11-18 13:41:37 +0100

    add hero section, text and booking component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Homepage from './pages/Homepage'
 
 const App = () => {
     const isOwnerPath = useLocation().pathname.includes('owner')
 
-    return <>{!isOwnerPath && <Navbar />}</>
+    return (
+        <>
+            {!isOwnerPath && <Navbar />}
+            <div className="min-h-[70vh]">
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                </Routes>
+            </div>
+        </>
+    )
 }
 
 export default App
