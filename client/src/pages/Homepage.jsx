c8ba6e3d3e9d3cf436ea64439abd8e9e801df9cb
William Mattsson <william.mattsson@iths.se>
2025-11-18 15:18:07 +0100

    add exclusive offers section

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,3 +1,4 @@
+import ExclusiveOffers from '../components/ExclusiveOffers'
 import FeatureDestination from '../components/FeatureDestination'
 import Hero from '../components/Hero'
 
@@ -6,6 +7,7 @@ const Homepage = () => {
         <>
             <Hero />
             <FeatureDestination />
+            <ExclusiveOffers />
         </>
     )
 }
