0b30dd9b18d9d594819db7b67490a8669bf8ad8c
William Mattsson <william.mattsson@iths.se>
2025-11-19 17:19:45 +0100

    add missing key prop and fix bug with room details id = undefined

--- client/src/components/FeatureDestination.jsx
+++ client/src/components/FeatureDestination.jsx
@@ -15,7 +15,7 @@ const FeatureDestination = () => {
 
             <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
                 {roomsDummyData.slice(0, 4).map((room, index) => (
-                    <HotelCard key={room.id} room={room} index={index} />
+                    <HotelCard key={room._id} room={room} index={index} />
                 ))}
             </div>
 
--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -7,6 +7,7 @@ const StarRating = ({ rating = 4 }) => {
                 .fill('')
                 .map((_, index) => (
                     <img
+                        key={index}
                         src={
                             rating > index
                                 ? assets.starIconFilled
