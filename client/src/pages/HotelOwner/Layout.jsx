70a5c84a3d0147b9e16dc41e7b10d94949b33152
William Mattsson <william.mattsson@iths.se>
2025-11-19 15:38:32 +0100

    add layout for owner with navbar and sidebar

---
+++ client/src/pages/HotelOwner/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/HotelOwner/Navbar'
+import Sidebar from '../../components/HotelOwner/Sidebar'
+
+export const Layout = () => {
+    return (
+        <div className="flex flex-col h-screen">
+            <Navbar />
+            <div className="flex h-full">
+                <Sidebar />
+                <div className="flex-1">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    )
+}
