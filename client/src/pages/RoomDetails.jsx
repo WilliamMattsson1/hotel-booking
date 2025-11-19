5e82c50df5d9d264f99bf96508a1a6b5f22aad7a
William Mattsson <william.mattsson@iths.se>
2025-11-19 13:47:44 +0100

    add room desc and host information

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, facilityIcons, roomsDummyData } from '../assets/assets'
+import {
+    assets,
+    facilityIcons,
+    roomCommonData,
+    roomsDummyData
+} from '../assets/assets'
 import StarRating from '../components/StarRating'
 
 const RoomDetails = () => {
@@ -164,6 +169,63 @@ const RoomDetails = () => {
                 </form>
 
                 {/* Common Specifications */}
+                <div className="mt-25 space-y-4">
+                    {roomCommonData.map((spec, index) => (
+                        <div key={index} className="flex items-start gap-2">
+                            <img
+                                src={spec.icon}
+                                alt={`${spec.title}-icon`}
+                                className="w-6.5"
+                            />
+                            <div>
+                                <p className="text-base">{spec.title}</p>
+                                <p className="text-gray-500">
+                                    {spec.description}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div>
+                    <p
+                        className="max-w-3xl border-y border-gray-300 my-15
+                        py-10
+                        text-gray-500"
+                    >
+                        Guests will be allocated on the ground floor according
+                        to availability. You get a comfotable Two bedroom
+                        apartment with a true city feeling. The price quoted is
+                        for two guests, at the guest slot please mark the number
+                        of guests to get the exact price for groups. The Guests
+                        will be allocated ground floor. The Guests will be
+                        allocated ground floor. The Guests will be allocated
+                        ground floor.
+                    </p>
+                </div>
+
+                {/* Hosted by */}
+                <div className="flex flex-col items-start gap-4">
+                    <div className="flex gap-4">
+                        <img
+                            src={room.hotel.owner.image}
+                            alt="Host"
+                            className="h-14 w-14 md:h-18 md:w-18 rounded-full"
+                        />
+                        <div>
+                            <p className="text-lg md:text-xl">
+                                Hosted by {room.hotel.name}
+                            </p>
+                            <div className="flex items-center mt-1">
+                                <StarRating />
+                                <p className="ml-2">200+ reviews</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="px-6 py-2.5 mt-4 rounded text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer">
+                        Contact Now
+                    </button>
+                </div>
             </div>
         )
     )
